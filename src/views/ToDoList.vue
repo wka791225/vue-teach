66bee3bb10cb39c7724e0d81e88988e809bf2c8c
wka791225 <93628110+wka791225@users.noreply.github.com>
2024-01-04 13:58:04 +0800

    feat:加入sweet alart 跟編輯事項

--- src/views/ToDoList.vue
+++ src/views/ToDoList.vue
@@ -5,19 +5,29 @@
 -->
 
 <script>
+import Swal from 'sweetalert2'
 export default {
 data() {
   return {
     addText:'',
     endTime: '',
     toDoListArr:[
-
+      {
+        id: 1,
+        toDo: '第一筆',
+        checkThis: false,
+        // 編輯開關
+        editIng: false,
+        logTime: '2023-12-31',
+        endTime: '2024-02-02',
+        // 編輯文字
+        newToDo: '',
+      }
     ],
   };
 },
 // 當網頁載入時會觸發
 mounted() {
-
   // 將sessionStorage的資料放入toDoListArr內 因為有轉成JSON格式所以要再轉回來
   if(localStorage.getItem('toDoList')){
     this.toDoListArr = JSON.parse(localStorage.getItem('toDoList'));
@@ -27,11 +37,25 @@ mounted() {
 methods:{
   // 新增功能
   addList() {
-    // 拿去當下時間
-    let date = new Date().toISOString().split('T');
+    Swal.fire({
+  title: "是否要新增",
+  icon: "question",
+  showCancelButton: true,
+  confirmButtonColor: "#3085d6",
+  confirmButtonText: "是",
+  cancelButtonColor: "#d33",
+  cancelButtonText: "否",
+}).then((result) => {
+  if (result.isConfirmed) {
+
+     // 拿去當下時間
+     let date = new Date().toISOString().split('T');
     const { addText, toDoListArr, endTime } = this;
     // 檢測是否為空字串
-    if(!addText && !endTime) return;
+    if(!addText || !endTime) return  Swal.fire({
+      title: "請填寫代辦事項及事項最後時間",
+      icon: "error"
+    });
     // session清空
     // sessionStorage.removeItem('toDoList');
     // 抓去array裡面的id最大數+1當作是下一個array的id 如果沒有的話則 id 給他1
@@ -40,14 +64,33 @@ methods:{
       id: listId,
       toDo: addText,
       checkThis: false,
+      // 編輯開關
+      editIng: false,
       logTime: date[0],
       endTime: endTime,
+      // 編輯的文字
+      newToDo: '',
     });
     this.addText = '';
+    this.endTime = '';
     // 將新的資料存入SESSION，將資料轉乘json格式儲存至SESSION內
     // sessionStorage.setItem('toDoList', JSON.stringify(toDoListArr));
      // 將新的資料存入localStorage，將資料轉乘json格式儲存至localStorage內
     localStorage.setItem('toDoList', JSON.stringify(toDoListArr));
+
+    Swal.fire({
+      title: "儲存成功",
+      icon: "success"
+    });
+  }
+});
+
+
+   
+  },
+  editList(id) {
+    console.log(id);
+
   },
   // 刪除功能
   deleteList(id) {
@@ -59,7 +102,21 @@ methods:{
   //  再放進localStorage內就可以完成刪除
    localStorage.setItem('toDoList', JSON.stringify(deletList));
     console.log(deletList);
-  }
+  },
+  // 修改觸發
+  starEdit(item){
+    // 編輯開關打開
+    item.editIng = !item.editIng;
+    // 舊的事項放入newToDo
+    item.newToDo = item.toDo;
+  },
+
+  topEditst(item) {
+    item.editIng = !item.editIng;
+    item.toDo = item.newToDo;
+// item.newToDo = '';
+  },
+
 }
 };
 </script>
@@ -92,13 +149,14 @@ methods:{
           <div class="w-full grid grid-cols-5 items-center">
             <div>
               <!-- 執行結束，勾選起來後 要把刪除按鈕拿掉 -->
-              <input v-model="item.checkThis" class="ml-3" type="checkbox" >
+              <input v-model="item.checkThis" class="ml-3" type="checkbox" @change="">
             </div>
-            <span class="grid grid-cols-subgrid">{{ item.toDo }}</span>
+            <span v-if="!item.editIng" class="grid grid-cols-subgrid" @click="starEdit(item)">{{ item.toDo }}</span>
+            <input v-else v-model="item.newToDo"  @blur="topEditst(item)" @keyup.enter="topEditst(item)" type="text" placeholder="修改事項">
+            
             <span>{{ item.endTime ?? '' }}</span>
             <span>{{ item.logTime }}</span>
-
-            <button class="bg-gradient-to-b from-red-500  to-orange-500 p-3 rounded-xl m-3 text-white" type="button" @click="deleteList(item.id)"><font-awesome-icon :icon="['fas', 'trash-can']" /></button>
+              <button class="bg-gradient-to-b from-red-500  to-orange-500 p-3 rounded-xl m-3 text-white" type="button" @click="deleteList(item.id)"><font-awesome-icon :icon="['fas', 'trash-can']" /></button>
           </div>
         </div>
       </div>
