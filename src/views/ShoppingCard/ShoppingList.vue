95186ad12d8cc393ccac48d0ae7d936036c46164
wka791225 <93628110+wka791225@users.noreply.github.com>
2024-02-23 18:20:54 +0800

    fix:加入列表

--- src/views/ShoppingCard/ShoppingList.vue
+++ src/views/ShoppingCard/ShoppingList.vue
@@ -1,1 +1,21 @@
 <!-- 購物車列表頁 -->
+<script></script>
+
+<template>
+  <div>
+    <h1>購物車列表</h1>
+    <div class="flex flex-wrap">
+      <div class="w-1/4 p-4">
+        <div class="bg-white p-4 rounded-lg shadow-lg">
+          <h2>商品名稱</h2>
+          <p>商品價格</p>
+          <p>商品數量</p>
+          <button>刪除</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+</style>
